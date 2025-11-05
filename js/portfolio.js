05969b22f29475e2168bd701546a0defc46a0ffa
Ani <u24808840@tuks.co.za>
2025-11-05 21:40:48 +0200

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -60,4 +60,73 @@ window.addEventListener("keydown", function(e) {
     var press = new Audio('sound/Lizard.mp3');
     press.play();
     press.volume = 1;
-})
+});
+
+$('#string01').mouseover(function(e) {
+    $('#blank').html("Header");
+});
+
+$('#string02').mouseover(function(e) {
+    $('#blank').html("About Me");
+});
+
+$('#string03').mouseover(function(e) {
+    $('#blank').html("First Year");
+});
+
+$('#string04').mouseover(function(e) {
+    $('#blank').html("Second Year");
+});
+
+$('#string05').mouseover(function(e) {
+    $('#blank').html("Comic");
+});
+
+$('#string06').mouseover(function(e) {
+    $('#blank').html("Final Notes");
+});
+
+$('#string07').mouseover(function(e) {
+    $('#blank').html("Character Design");
+});
+
+$('#string08').mouseover(function(e) {
+    $('#blank').html("Blender Character Design");
+});
+
+$('#string09').mouseover(function(e) {
+    $('#blank').html("Enviromental Design");
+});
+
+$('#string10').mouseover(function(e) {
+    $('#blank').html("Enviromental Design Continued");
+});
+
+var glowOp = 1;
+var glowDir = "DOWN";
+
+function glowPulse() {
+    var glow = document.querySelectorAll(".glow");
+    if (glowDir == "UP") {
+        glowOp += 0.01;
+        for (var i = 0; i < glow.length; i++) {
+            glow[i].style.setProperty("opacity",glowOp);
+        }
+
+        if (glowOp >= 1) {
+            glowDir = "DOWN";
+        }
+    }
+    else {
+        glowOp -= 0.01;
+        for (var i = 0; i < glow.length; i++) {
+            glow[i].style.setProperty("opacity",glowOp);
+        }
+
+        if (glowOp <= 0) {
+            glowDir = "UP";
+        }
+    }
+}
+
+setInterval(glowPulse, 25);
